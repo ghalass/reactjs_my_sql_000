14160b9624042cdf98b9a59a4deaa41716204c13
MED SALEM GHALASS <msghalas@gmail.com>
2023-08-01 11:57:33 +0000

    update home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { Button, Card } from "react-bootstrap";
 
 function Home() {
-  return <div>Welcome To Home page</div>;
+  return (
+    <div>
+      <h6>Welcome To Home page</h6>
+      <hr />
+      <Card>
+        <Card.Header>Featured</Card.Header>
+        <Card.Body>
+          <Card.Title>Special title treatment</Card.Title>
+          <Card.Text>
+            With supporting text below as a natural lead-in to additional
+            content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 }
 
 export default Home;
